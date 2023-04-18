007d88f55f54bd37bc849580b79a0eb183c6c4c0
Admin <221250@astanait.edu.kz>
2023-04-18 08:43:23 +0600

    A new created class "MyArrayList" with implementation of "List<E>", where defined new variables Object[] and size.

---
+++ MyArrayList.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MyArrayList<E> implements List<E> {
+
+    private Object[] elements;
+    private int size;
+
+    public MyArrayList() {
+        elements = new Object[10];
+        size = 0;
+    }}
+
+
+
+
