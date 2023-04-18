9ff5b8eea2d9f9ccab0e844a8e52c59f301af122
Admin <221250@astanait.edu.kz>
2023-04-18 09:45:55 +0600

    A new method "add(E element)", that check the size of array and if size and length of elements is equal doubled the size in new array and add previous elements there and include new

--- MyArrayList.java
+++ MyArrayList.java
@@ -8,7 +8,29 @@ public class MyArrayList<E> implements List<E> {
     public MyArrayList() {
         elements = new Object[10];
         size = 0;
-    }}
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    public boolean add(E element) {
+        if (size == elements.length) {
+            Object[] newElements = new Object[elements.length * 2];
+            System.arraycopy(elements, 0, newElements, 0, elements.length);
+            elements = newElements;
+        }
+        elements[size++] = element;
+        return true;
+    }
+}
+
 
 
 
