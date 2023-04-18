0df960ac8194b2f082a9d460458745266d30a3f7
Admin <221250@astanait.edu.kz>
2023-04-18 18:56:28 +0600

    A new method "remove(int index)". If the index parameter is out of the valid range of the list's indices, an IndexOutOfBoundsException is thrown. Otherwise, the method retrieves the element at the specified index, shifts all subsequent elements to the left by one position, sets the last element to null, and returns the removed element.

---
+++ MyLinkedList.java
@@ -0,0 +1,117 @@
+import java.util.*;
+
+public abstract class MyLinkedList<E> implements List<E> {
+private Node<E> head;
+private Node<E> tail;
+private int size;
+
+private static class Node<E> {
+    E element;
+    Node<E> next;
+    Node<E> prev;
+
+    public Node(E element, Node<E> next, Node<E> prev) {
+        this.element = element;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+    public boolean add(E element) {
+        Node<E> newNode = new Node<>(element, null, tail);
+        if (tail == null) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        size++;
+        return true;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> current = head;
+        if (index < size / 2) {
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = tail;
+            for (int i = size - 1; i > index; i--) {
+                current = current.prev;
+            }
+        }
+        return current.element;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> current = head;
+        if (index == 0) {
+            head = head.next;
+        } else if (index == size - 1) {
+            current = tail;
+            tail = tail.prev;
+            tail.next = null;
+        } else {
+            if (index < size / 2) {
+                for (int i = 0; i < index; i++) {
+                    current = current.next;
+                }
+            } else {
+                current = tail;
+                for (int i = size - 1; i > index; i--) {
+                    current = current.prev;
+                }
+            }
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+        size--;
+        return current.element;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean contains(Object obj) {
+        Node<E> current = head;
+        while (current != null) {
+            if (obj.equals(current.element)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private Node<E> current = head;
+
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            public E next() {
+                E element = current.element;
+                current = current.next;
+                return element;
+            }
+
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+}
