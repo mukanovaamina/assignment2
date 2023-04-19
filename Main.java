f29f61927eef95c8267c8c07eced66363c238ab4
Admin <221250@astanait.edu.kz>
2023-04-19 21:37:01 +0600

    Test the get() method

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 import java.util.*;
 public class Main {
-public static void main(String[] args) {
-    MyArrayList<Integer> myList = new MyArrayList<Integer>();
-    myList.add(5);
-    myList.add(10);
-    myList.add(15);
-    myList.add(20);
-    System.out.println("Original list: " + myList);
-}
+    public static void main(String[] args) {
+        MyArrayList<Integer> myList = new MyArrayList<Integer>();
+        myList.add(5);
+        myList.add(10);
+        myList.add(15);
+        myList.add(20);
+        System.out.println("Original list: " + myList);
+        System.out.println("Element at index 2: " + myList.get(2));
+    }
 }
