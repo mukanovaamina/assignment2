7c6ebbc005f58d80b5facef4c8c1c7684bf23d75
Admin <221250@astanait.edu.kz>
2023-04-19 21:31:43 +0600

    testing of add method

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+public static void main(String[] args) {
+    MyArrayList<Integer> myList = new MyArrayList<Integer>();
+    myList.add(5);
+    myList.add(10);
+    myList.add(15);
+    myList.add(20);
+    System.out.println("Original list: " + myList);
+}
+}
