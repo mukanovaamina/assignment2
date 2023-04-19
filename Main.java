309238480a2ca3c3e227a3292fa1c715f145ca4d
Admin <221250@astanait.edu.kz>
2023-04-19 21:49:59 +0600

    Test the remove() method

--- Main.java
+++ Main.java
@@ -10,6 +10,8 @@ public class Main {
         System.out.println("Element at index 2: " + myList.get(2));
         System.out.println("Does list contain 15? " + myList.contains(15));
         System.out.println("Does list contain 25? " + myList.contains(25));
+        System.out.println("Removing element at index 1: " + myList.remove(1));
+        System.out.println("Updated list: " + myList);
     }
 
 }
